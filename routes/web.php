18616462a67e20470bab57429f99aae79f04a55d
Ubuntu <ubuntu@ip-172-31-37-0.ap-northeast-1.compute.internal>
2020-07-13 08:16:43 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');
+
 // ユーザ登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
