2819cb9d3cf65eea15aa0feddfc47c081e5d3e8e
Ubuntu <ubuntu@ip-172-31-37-0.ap-northeast-1.compute.internal>
2020-07-20 13:29:15 +0000

    navbar

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -20,7 +20,7 @@
                             <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
                             
                             {{-- お気に入り一覧ページ --}}
-                            <li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorites', ['user' => Auth::id()])  !!}</li>
+                            <li class="dropdown-item">{!! link_to_route('users.favorites', 'Favorites', ['id' => Auth::id()])  !!}</li>
                             
                             <li class="dropdown-divider"></li>
                             {{-- ログアウトへのリンク --}}
